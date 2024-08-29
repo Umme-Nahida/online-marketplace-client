c5fbb6fffb6afc635278e72925c649d8fbdb0ad7
Umme-Nahida <mysuccessmission@gmail.com>
2024-08-29 20:39:07 +0600

    some changes in job details page

--- src/pages/jobDetails/JobDetails.jsx
+++ src/pages/jobDetails/JobDetails.jsx
@@ -23,7 +23,7 @@ const JobDetails = () => {
         const bidInfo = { myEmail, buyerEmail, minPrice, maxPrice, deadline, category,description,jobTitle,status }
         console.log(bidInfo)
         
-        fetch('http://localhost:5000/storeBidJobs', {
+        fetch('https://assignment-11-server-orpin.vercel.app/storeBidJobs', {
             method: 'POST',
             headers: {
                 'content-type': 'application/json'
